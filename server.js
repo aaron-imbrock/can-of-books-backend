02ddf7c232e0cae85ea03dbf2b9f40a70168671a
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-04-24 18:03:31 -0500

    setting up server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
 
@@ -16,3 +18,13 @@ app.get('/test', (request, response) => {
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not available');
+});
+
+// ERROR
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
